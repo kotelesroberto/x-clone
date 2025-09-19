839b2f11eb36b7e32141008469978f7c919f8d76
Köteles Róbert <koteles.roberto@gmail.com>
2025-09-19 11:57:19 +0200

    Material UI added

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,24 @@
+// import { useState } from "react";
+import XIcon from '@mui/icons-material/X';
+
+
+const Login = () => {
+   
+    return (
+        <>
+            <section>
+                <div>
+                    {/** LOGO */}
+                    <XIcon />
+                </div>
+
+                <aside>
+                    {/** LOGIN FORM */}
+
+                </aside>
+            </section>
+        </>
+    );
+}
+
+export default Login;
